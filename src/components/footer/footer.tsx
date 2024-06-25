07f563d51e7a2b65b87cbe995e9ff9d2e571a152
amirmastermind <158269526+amirmastermind@users.noreply.github.com>
2024-06-25 18:58:57 -0500

    footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -13,14 +13,9 @@ export default function Footer(): JSX.Element {
             src={footerLogo}
             alt="footer logo"
           />
-<a className={classes.footer__description}> Trading Cryptocurrencies is highly speculative, carries a level of risk and may not be suitable for all investors. You may lose some or all of your invested capital, therefore you should not speculate with capital that you cannot afford to lose. The content on this site should not be considered investment advice. Investing is speculative. When investing your capital is at risk.</a>
+<center className={classes.footer__description}> Trading Cryptocurrencies is highly speculative, carries a level of risk and may not be suitable for all investors. You may lose some or all of your invested capital, therefore you should not speculate with capital that you cannot afford to lose. The content on this site should not be considered investment advice. Investing is speculative. When investing your capital is at risk.</center>
    </div>
-     <a href="/" className={classes.footer__x__logo}>
-          <img src={xLogo} alt="x/twitter logo" />
 
-      
-  </a>
-      </div>
     </footer>
   );
 }
