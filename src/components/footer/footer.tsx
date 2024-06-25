78dcd2a792a084fa2a70b7f7d1c2ca529a9af758
amirmastermind <158269526+amirmastermind@users.noreply.github.com>
2024-06-25 16:30:42 -0500

    footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -15,10 +15,10 @@ export default function Footer(): JSX.Element {
           />
           <p className={classes.footer__logo__title}>THIS IS $VODKA THE $BEER KILLER!</p>
         </div>
+<a className={classes.footer__description}> Trading Cryptocurrencies is highly speculative, carries a level of risk and may not be suitable for all investors. You may lose some or all of your invested capital, therefore you should not speculate with capital that you cannot afford to lose. The content on this site should not be considered investment advice. Investing is speculative. When investing your capital is at risk.</a>
         <a href="/" className={classes.footer__x__logo}>
           <img src={xLogo} alt="x/twitter logo" />
 
-<div className={classes.footer__description}> Trading Cryptocurrencies is highly speculative, carries a level of risk and may not be suitable for all investors. You may lose some or all of your invested capital, therefore you should not speculate with capital that you cannot afford to lose. The content on this site should not be considered investment advice. Investing is speculative. When investing your capital is at risk.</div>
       
   </a>
       </div>
